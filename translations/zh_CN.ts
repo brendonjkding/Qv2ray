9e19e489cdf29381f449857f2f4ddb5656e37561
Brendon <brendonjkdingnew@gmail.com>
2022-04-03 14:06:49 +0800

    Revert fucking "zh_CN.ts: #962"
    This reverts fucking commit cd76673e98b98e8014acf6b0111e14e56a0f7e52.

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -1784,11 +1784,11 @@ If your V2Ray core filename happened to be &apos;qv2ray&apos;-something, you are
     </message>
     <message>
       <source>Adapt Dark Theme</source>
-      <translation>使用浅色界面图标</translation>
+      <translation>适应暗色主题</translation>
     </message>
     <message>
       <source>Adapt Dark Tray Theme</source>
-      <translation>使用浅色托盘图标</translation>
+      <translation>适应暗色托盘主题</translation>
     </message>
     <message>
       <source>V2Ray Core Settings</source>
